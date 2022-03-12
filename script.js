3cb17882ce1f8b72518751dea08f32faeb00fba6
Thefany Silva <thefanyja@gmail.com>
2022-03-12 12:34:41 -0500

    updated past search buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var searchField = document.querySelector('#searchField');
+const searchField = document.querySelector('#searchField');
+const clearEl = document.getElementById("clear-history");
 const cityInput = document.querySelector("#city")
 const inputValue = document.querySelector(".inputValue");
 const history = document.querySelector(".history");
@@ -9,14 +10,15 @@ const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 const uv = document.querySelector(".uv");
-let wDisplay = document.querySelector("#wDisplay");
+const wDisplay = document.querySelector("#wDisplay");
+const forecastTitle = document.querySelector("#fiveDay");
 const forcast = document.querySelector("#forcastContainer");
-let pastSearchBtn = document.querySelector("#pastSearchBtn");
+const pastSearchBtn = document.querySelector("#pastSearchBtn");
 let cities = [];
 
 let formSubmitHandler = function (event) {
     event.preventDefault();
-    let city = cityInput.value.trim();
+    var city = cityInput.value.trim();
     if (city) {
         getWeather(city);
         getForcast(city);
@@ -30,7 +32,7 @@ let formSubmitHandler = function (event) {
 }
 // getting api  for 5-day forcast
 let getForcast = function (city) {
-    const requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + "&units=imperal&appid=1483407154edd2e0bda63bf21519c92c";
+    const requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + "&units=imperial&appid=1483407154edd2e0bda63bf21519c92c";
 
     fetch(requestUrl)
         .then(function (response) {
@@ -38,11 +40,16 @@ let getForcast = function (city) {
         })
         .then((data) => {
             console.log(data);
+forcast.innerHTML ='';
 
             // empty div to create elements and append
             let cName = document.createElement('h3');
             cName.textContent = data.city.name;
+            cName.classList = "card-body text-center";
             forcast.append(cName);
+            forecastTitle.textContent="5-Day Forecast";
+
+            
             // loop for 5-day forcast
             for (let i = 5; i < data.list.length; i = i + 8) {
                 // create element for empty div in html
@@ -50,12 +57,22 @@ let getForcast = function (city) {
                 let temperature = document.createElement('p');
                 let humid = document.createElement('p');
                 let weatherIcon = document.createElement("img");
+                let forecastEl=document.createElement("div");
+            forecastEl.classList = "card bg-primary text-light m-2";
+                forcastDate.classList = "card-header text-center"
+                forecastEl.appendChild(forcastDate);
+         
                 // let tempF = (data.list[i].main.temp - 273.15) * 1.80 + 32;
                 // tempF = Math.floor(tempF);
                 forcastDate.textContent = moment.unix(data.list[i].dt).format("MMM D, YYYY");
-                temperature.textContent = ("Temperature: " + data.list[i].main.temp);
+                forcastDate.classList = "card-header text-center";
+                temperature.textContent = ("Temperature: " + data.list[i].main.temp) + "°F";
+                temperature.classList = "card-body text-center";
                 humid.textContent = ("Humdity: " + data.list[i].main.humidity + "%");
+                humid.classList = "card-body text-center";
                 weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon + ".png"}`)
+                weatherIcon.classList = "card-body text-center";
+                
 
                 forcast.append(forcastDate);
                 forcast.append(weatherIcon);
@@ -77,13 +94,17 @@ let getWeather = function (city) {
         })
 }
 
+//Display weather in empty div
 let showWeather = function (weather) {
-
+    wDisplay.innerHTML='';
     let currentDate = document.createElement("span");
+    currentDate.classList = "list-group-item";
     currentDate.textContent = moment(weather.dt.value).format("MMM D, YYYY");
     wDisplay.appendChild(currentDate);
 
-    let wIcon = document.createElement("img")
+
+    let wIcon = document.createElement("img");
+    wIcon.classList ="list-group-item";
     wIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
     wDisplay.appendChild(wIcon);
 
@@ -93,11 +114,14 @@ let showWeather = function (weather) {
 
 
     tempValue.textContent = ("Temperature: " + weather.main.temp + "°F");
+    tempValue.classList = "list-group-item";
     humidityValue.textContent = ("Humdity: " + weather.main.humidity + "%");
+    humidityValue.classList = "list-group-item";
     windValue.textContent = ("Wind: " + weather.wind.speed + "MPH");
-
+    windValue.classList = "list-group-item";
     let lat = weather.coord.lat;
     let lon = weather.coord.lon;
+
     getUv(lat, lon)
 
 };
@@ -112,13 +136,16 @@ let getUv = function (lat, lon) {
         });
 }
 
+//Append UV index on current weather display
 let showUv = function (index) {
-
+    // uvIndex.innerHTML='';
     uvIndex = document.createElement("p");
     uvIndex.textContent = "UV Index: "
+    uvIndex.classList = "list-group-item";
 
     uvValue = document.createElement("span")
     uvValue.textContent = index.value
+    uvValue.classList = "list-group-item";
 
     if (index.value <= 2) {
         uvValue.classList = "good"
@@ -138,6 +165,7 @@ let pastSearch = function (pastSearch) {
 
     let pastSearchEl = document.createElement("button");
     pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-auto btn-light border p-2";
     pastSearchEl.setAttribute("data-city", pastSearch)
     pastSearchEl.setAttribute("type", "submit");
 
@@ -155,6 +183,6 @@ let saveSearch = function () {
     localStorage.setItem("cities", JSON.stringify(cities));
 }
 searchField.addEventListener('submit', formSubmitHandler);
-pastSearchBtn.addEventListener("click", pastSearchHand)
+pastSearchBtn.addEventListener("click", pastSearchHand);
 
 
